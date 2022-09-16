09b274c3001fb2deed0587b92ae8e8a0a2b1ecc4
Jorge Leonardo Herrera Ortiz <jorge_skidrow@hotmail.com>
2022-09-16 17:39:53 -0500

    fix(margin): margin fixed

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,7 +6,7 @@ function HomePage() {
         <DefaultLayout>
             <div className="row">
                 <div className="col">
-                    <div className="d-flex flex-column align-items-center">
+                    <div className="d-flex flex-column align-items-center mt-5 mb-3">
                         <h4>Flotas La Macarena</h4>
                         <small>NIT: 800 220 154</small>
                         <div>
@@ -14,7 +14,7 @@ function HomePage() {
                             <strong>Segmento: </strong><small>A</small>
                         </div>
                     </div>
-                    <nav>
+                    <nav className="mb-4">
                         <div className="nav nav-tabs justify-content-between" id="nav-tab" role="tablist">
                             <button className="nav-link active" id="nav-operation-tab" data-bs-toggle="tab" data-bs-target="#nav-operation" type="button" role="tab" aria-controls="nav-operation" aria-selected="true">Operación</button>
                             <button className="nav-link" id="nav-indicator-tab" data-bs-toggle="tab" data-bs-target="#nav-indicator" type="button" role="tab" aria-controls="nav-indicator" aria-selected="false">Indicadores</button>
@@ -24,30 +24,30 @@ function HomePage() {
                     <div className="tab-content" id="nav-tabContent">
                         <div className="tab-pane fade show active" id="nav-operation" role="tabpanel" aria-labelledby="nav-operation-tab" tabIndex="0">
                             <div className="row">
-                                <div className="col-12">
+                                <div className="col-12 mb-4">
                                     <p className="text-primary">Indique la línea de crédito que quiere reconsiderar</p>
                                 </div>
                                 <div className="col-12">
                                     <div className="d-flex justify-content-center">
                                         <div className="card">
                                             <div className="card-body">
-                                                <i class="bi bi-wallet2" style={{ fontSize: '6em' }}></i>
+                                                <i className="bi bi-wallet2" style={{ fontSize: '3em' }}></i>
                                                 <h5 className="card-title">Cartera ordinaria</h5>
                                                 <a href="/#" className="btn btn-light text-secondary">Continuar</a>
                                             </div>
                                         </div>
                                         <div className="card">
                                             <div className="card-body">
-                                                <i class="bi bi-shop" style={{ fontSize: '6em' }}></i>
+                                                <i className="bi bi-shop" style={{ fontSize: '3em' }}></i>
                                                 <h5 className="card-title">Leasing</h5>
                                                 <a href="/#" className="btn btn-light text-secondary">Continuar</a>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div className="col-12">
-                                    <div className="alert alert-warning d-flex align-items-center text-warning" role="alert">
-                                        <div><i class="bi bi-info-circle"></i> Las demás líneas de crédito no estarán por el momento.</div>
+                                <div className="col-12 mt-5 mb-5">
+                                    <div className="alert alert-warning text-warning" role="alert">
+                                        <div><i className="bi bi-info-circle"></i> Las demás líneas de crédito no estarán por el momento.</div>
                                     </div>
                                 </div>
                             </div>
