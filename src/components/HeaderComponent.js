6cd5376928ec112dd94a1e7bdb8e54451702e787
Jorge Leonardo Herrera Ortiz <jorge_skidrow@hotmail.com>
2022-09-16 15:52:14 -0500

    feat(components - DefaultComponent): default layout added

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+function HeaderComponent() {
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+    
+    return (
+        <nav className="navbar navbar-expand-lg bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/#">Banco de Occidente</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={() => handleNavCollapse()}>
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse justify-content-end`} id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            Carlos Gómez
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default HeaderComponent;
