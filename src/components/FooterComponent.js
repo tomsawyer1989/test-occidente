29e517a7ef697d4d08d078eb99e516d8dce0c789
Jorge Leonardo Herrera Ortiz <jorge_skidrow@hotmail.com>
2022-09-17 15:04:15 -0500

    fix(FooterComponent): responsive fixed

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -3,7 +3,10 @@ import React from "react";
 function FooterComponent() {
     return (
         <nav className="navbar bg-light">
-            <div className="w-100 d-flex justify-content-end">
+            <div className="w-100 d-xl-none d-flex justify-content-center">
+                <img width={120} height={30} src={require('../assets/images/grupaval.png')} alt="grupaval" />
+            </div>
+            <div className="w-100 d-none d-xl-flex justify-content-end">
                 <img width={120} height={30} src={require('../assets/images/grupaval.png')} alt="grupaval" />
             </div>
         </nav>
