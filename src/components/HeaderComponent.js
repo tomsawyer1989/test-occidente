9abea2360d338b1519bc7acc61484ca80cb3eebb
Jorge Leonardo Herrera Ortiz <jorge_skidrow@hotmail.com>
2022-09-17 03:56:57 -0500

    feat(HeaderComponent): avatar client added

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -5,6 +5,7 @@ import { fetchClient } from "../services/clients";
 
 function HeaderComponent() {
     const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+    const [isDropOpen, setIsDropOpen] = useState(true);
     const client = useSelector(selectClient);
     const dispatch = useDispatch();
 
@@ -12,22 +13,32 @@ function HeaderComponent() {
         dispatch(fetchClient());
     }, []);
 
-    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
-    
     return (
         <nav className="navbar navbar-expand-lg bg-light">
             <div className="container-fluid">
                 <a className="navbar-brand" href="/#">
                     <img width={220} height={50} src={require('../assets/images/bancoccidente.png')} alt="bancoccidente" />
                 </a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={() => handleNavCollapse()}>
+                <button className="navbar-toggler" type="button" onClick={() => setIsNavCollapsed(!isNavCollapsed)} aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse justify-content-end`} id="navbarNav">
                     {client && <ul className="navbar-nav">
-                        <li className="nav-item d-flex flex-column">
-                            <strong>{ client.gerenteRelacion }</strong>
-                            <small className="text-secondary">Gerente de Relación</small>
+                        <li className="nav-item d-flex">
+                            <div className="d-flex flex-column justify-content-center">
+                                <strong>{client.gerenteRelacion}</strong>
+                                <small className="text-secondary">Gerente de Relación</small>
+                            </div>
+                            <div className="dropdown">
+                                <button className="btn dropdown-toggle rounded-circle" type="button" onClick={() => setIsDropOpen(!isDropOpen)}>
+                                    <i className="bi bi-person-fill" style={{ fontSize: '2em' }}></i>
+                                </button>
+                                <ul className={`position-absolute dropdown-menu${!isDropOpen ? " show" : ""}`} style={{ right: '0px' }}>
+                                    <li><a className="dropdown-item" href="/#">Opción 1</a></li>
+                                    <li><a className="dropdown-item" href="/#">Opción 2</a></li>
+                                    <li><a className="dropdown-item" href="/#">Opción 3</a></li>
+                                </ul>
+                            </div>
                         </li>
                     </ul>}
                 </div>
